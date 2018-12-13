fcd7740836c32cf4a68c72f7a3298a75e5b0f37a
Robert Klim <kontakt.robert.klim@gmail.com>
2018-12-13 18:28:32 +0100

    Draw points on the canvas

--- js/sketch.js
+++ js/sketch.js
@@ -1,7 +1,28 @@
+let xs = [];
+let ys = [];
+
 function setup() {
     createCanvas(400, 400);
 }
 
+function mousePressed() {
+    // normalize coordinates (values between 0-1)
+    let x = map(mouseX, 0, width, 0, 1);
+    let y = map(mouseY, 0, height, 1, 0);
+    
+    xs.push(x);
+    ys.push(y);
+}
+
 function draw() {
-    background(0);
+    background(255);
+    stroke(0);
+    strokeWeight(6);
+    for (let i = 0; i < xs.length; i++) {
+        // reverse normalization (values between 0 - height and 0 - width)
+        let px = map(xs[i], 0, 1, 0, width);
+        let py = map(ys[i], 0, 1, height, 0);
+        // draw a point
+        point(px, py);
+    }
 }
